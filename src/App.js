6416ae5a992e75191cc94e39332c2562c7464067
zhangwuyang-github <zhangwuyangyx@163.com>
2021-07-19 18:14:40 +0800

    使用electron进行包裹
    使用electron进行包裹

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
 import './App.scss'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import io from 'socket.io-client'
 
 import Home from './home/home'
 import Chat from './chat/chat'
-import Process from './process/process'
 
 const socket = io.connect('/')
 
 const Appmain = (props) => {
   return (
     <React.Fragment>
-      <div className="right">
+      <div className="content">
         <Chat
           username={props.match.params.username}
           roomname={props.match.params.roomname}
           socket={socket}
         ></Chat>
-      </div>
-      <div className="left">
-        <Process></Process>
       </div>
     </React.Fragment>
   );
